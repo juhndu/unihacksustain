83293b6d13f1026b4c917b0c49a65668013f22d1
Eugene Daily <33009870+golemXV@users.noreply.github.com>
2018-08-04 22:45:29 +1000

    Mostly style changes

--- sustain/frontend/src/components/Card.vue
+++ sustain/frontend/src/components/Card.vue
@@ -4,17 +4,29 @@
             <img :src="data.image_url">
         </div>
         <div class="bottom-content">
-            <div class="bottom-content-title">
-                <h4>{{ data.name }}</h4>
-                <div class="right-panel">
-                    
+                <div class="bottom-content-title align-top">
+                    <h2>{{ data.name }}</h2>
+                    <h3>{{ data.location }}</h3>
                 </div>
-            </div>
-            <star-rating 
-                v-model="rating"
-                v-bind:star-size="20"
-                v-bind:increment="0.5">
-            </star-rating>
+
+                <div class="align-bottom">
+                    <small>Sustainability: </small>
+                    <star-rating 
+                        v-model="rating"
+                        v-bind:star-size="20"
+                        v-bind:increment="0.5"
+                        :border-width="0"
+                        active-color="rgb(64, 207, 71)">
+                    </star-rating>
+                    <small>Zomato: </small>
+                    <star-rating 
+                        v-model="rating"
+                        v-bind:star-size="15"
+                        v-bind:increment="0.5"
+                        :border-width="0"
+                        active-color="rgb(64, 207, 71)" >
+                    </star-rating>
+                </div>
         </div>
     </div>
 </template>
@@ -42,12 +54,25 @@ export default {
 }
 </script>
 <style scoped>
+    .bottom-content{
+        height: 200px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+
     .bottom-content-title{
         display: flex;
+        flex-direction: column;
     }
-    h4{
+    h2{
         color: #4f4f4f;
         margin: 0px;
+    }
+    h3{
+        margin: 0px;
+        color: rgb(131, 131, 131);
     }
     .image{
         height: 150px;
@@ -58,17 +83,16 @@ export default {
         width: 100%;
     }
     .bottom-content{
-        height: 200px;
+
         padding: 15px;
     }
     .card{
         border-radius: 5px;
-        background-color: #FFFFFF;
+        background-color: rgb(232, 242, 232);
         box-shadow: 0 0 12px 0 hsla(0, 0%, 0%, 0.2);
         display: flex;
         flex-direction: column;
         text-align: left;
-
     }
     .card:hover{
         box-shadow: 0 0 12px 0 hsla(0, 0%, 0%, 0.5);
